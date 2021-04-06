9f639820f5b7af6a0224cb918c1f67a59250d642
Berkin Kule <berkinkule@gmail.com>
2021-04-06 14:24:38 +0300

    LINQ Intro

---
+++ Day7/ReCapProject/ConsoleUI/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Business.Concrete;
+using DataAccess.Concrete.InMemoryCarDal;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            (Türkçe)
+            Araba kiralama sistemi yazıyoruz.
+            Yepyeni bir proje oluşturunuz. Adı ReCapProject olacak. (Tekrar ve geliştirme projesi)
+            Entities, DataAccess, Business ve Console katmanlarını oluşturunuz.
+            Bir araba nesnesi oluşturunuz. "Car"
+            Özellik olarak : Id, BrandId, ColorId, ModelYear, DailyPrice, Description alanlarını ekleyiniz.
+            InMemory formatta GetById, GetAll, Add, Update, Delete oprasyonlarını yazınız.
+            Consolda test ediniz.
+                
+                
+            (English)
+            We will write Car Rental System.
+            Create a new project called ReCapProject
+            Create Entities, DataAccess, Business and Console layers.
+            Create a car object. Add features : Id, BrandId, ColorId, ModelYear, DailyPrice, Description.
+            Write the operations : GetById, GetAll, Add, Update, Delete in InMemory format.
+            */
+
+
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.CarId);
+            }
+        }
+    }
+}
---
+++ Day7/ReCapProject/DataAccess/Concrete/InMemoryCarDal/InMemoryCarDal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.InMemoryCarDal
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _car;
+
+        public InMemoryCarDal()
+        {
+            _car = new List<Car>
+            {
+                new Car{CarId=1,BrandId=1, ColorId=1, ModelYear ="2000", DailyPrice = 13, Description="Economy"},
+                new Car{CarId=2,BrandId=1, ColorId=1, ModelYear ="2013", DailyPrice = 30, Description="Standard"},
+                new Car{CarId=3,BrandId=1, ColorId=2, ModelYear ="2020", DailyPrice = 54, Description="Luxury"},
+                new Car{CarId=4,BrandId=2, ColorId=1, ModelYear ="2015", DailyPrice = 24, Description="Compact"},
+                new Car{CarId=5,BrandId=2, ColorId=2, ModelYear ="2017", DailyPrice = 33, Description="Standard"},
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _car.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _car.SingleOrDefault(c => c.CarId == car.CarId);
+            _car.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _car;
+        }
+
+        public List<Car> GetAllById(int carId)
+        {
+            return _car.Where(c => c.CarId == carId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _car.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
