aaf008a630b5a2850b6b17efb7b36da2df92e6a8
Berkin Kule <berkinkule@gmail.com>
2021-04-06 14:12:01 +0300

    CSharp Intro

---
+++ Day2/Day2Sample5/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Day2Sample5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*  
+            (Türkçe)
+            (Bir adet quiz sorusu yazınız. Bu soru bugüne kadar öğrendiklerimizi içermelidir. Doğru cevabı belirtmeyiniz.)
+                
+            ----- Quiz sorusu -----
+            Bir marketteki sebze-meyve reyonunda bulunan 4 adet ürünün, ürünleri ve fiyatları gösteren ve bu fiyatlar kilosu 5 Türk Lirası'ndan fazla ise 
+            pahalı olduğunu, kilosu 5 Türk Lirası'ndan az ise Ucuz olduğunu gösteren bir kod yazınız. 
+                
+                
+            (English)
+            (Write a quiz question. This question should include what we have learned so far. Do not specify the correct answer.)
+                
+            ----- My Quiz Question -----
+            Write a code that shows 4 products and their prices in fruit-vegetables section in a supermarket. Also, that code must show if the prices are more
+            than 5 Turkish Liras in per kilo as "Expensive" , and less than 5 Turkish Liras in per kilo as "Cheap".
+            */
+
+
+            GreenGrocer banana = new GreenGrocer();
+            banana.ProductName = "Banana";
+            banana.ProductPrice = 9.95;
+            banana.ProductType = "Fruit";
+
+            GreenGrocer tomato = new GreenGrocer();
+            tomato.ProductName = "Tomato";
+            tomato.ProductPrice = 4.99;
+            tomato.ProductType = "Vegetable";
+
+            GreenGrocer leek = new GreenGrocer();
+            leek.ProductName = "Leek";
+            leek.ProductPrice = 6.95;
+            leek.ProductType = "Vegetable";
+
+            GreenGrocer grapefruit = new GreenGrocer();
+            grapefruit.ProductName = "Grapefruit";
+            grapefruit.ProductPrice = 4.99;
+            grapefruit.ProductType = "Fruit";
+
+            GreenGrocer[] greengrocer = new GreenGrocer[] { banana, tomato, leek, grapefruit };
+
+            for (int i = 0; i < greengrocer.Length; i++)
+            {
+                if (greengrocer[i].ProductPrice > 5)
+                {
+                    Console.WriteLine(greengrocer[i].ProductName + " : " + greengrocer[i].ProductPrice + " TL/kg - Expensive " + greengrocer[i].ProductType);
+                }
+                else
+                {
+                    Console.WriteLine(greengrocer[i].ProductName + " : " + greengrocer[i].ProductPrice + " TL/kg - Cheap " + greengrocer[i].ProductType);
+                }
+            }
+        }
+    }
+
+    class GreenGrocer
+    {
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+        public string ProductType { get; set; }
+    }
+}
---
+++ Day2/Day2Sample6/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Day2Sample6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            (Türkçe)
+            Herhangi bir e-ticaret sistemine giriniz. Genellikle bir e - ticaret sistemine girdiğinizde ürünler listelenir. 
+            Siz de Urun(Product) isimli bir class oluşturup, oluşturduğunuz bir kaç ürünü bir diziye ekleyiniz.
+            Ürünlerinizi for, foreach ve while döngüleri ile ayrı ayrı listeleyiniz.
+            
+            
+            (English)
+            Enter any e-commerce system. Usually when you enter an e-commerce system, products are listed.
+            Create a class called Product(Product) and add a few products to an array.
+            List your products separately with for, foreach and while loops.
+            */
+
+
+            Product product1 = new Product();
+            product1.ProductName = "Roomba-i6";
+            product1.ProductPrice = 559.98;
+            product1.ProductDealer = "iRobot Store";
+
+            Product product2 = new Product();
+            product2.ProductName = "Humidifier";
+            product2.ProductPrice = 44.15;
+            product2.ProductDealer = "Upper Echelon";
+
+            Product product3 = new Product();
+            product3.ProductName = "Puzzle";
+            product3.ProductPrice = 19.99;
+            product3.ProductDealer = "Tzftoy";
+
+            Product[] products = new Product[] { product1, product2, product3 };
+
+            for (int i = 0; i < products.Length; i++)
+            {
+                Console.WriteLine(products[i].ProductName + " : " + products[i].ProductPrice + " by " + products[i].ProductDealer);
+            }
+
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.ProductName + " : " + product.ProductPrice + " by " + product.ProductDealer);
+            }
+
+            int x = 0;
+            while (x < products.Length)
+            {
+                Console.WriteLine(products[x].ProductName + " : " + products[x].ProductPrice + " by " + products[x].ProductDealer);
+                x++;
+            }
+        }
+    }
+    class Product
+    {
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+        public string ProductDealer { get; set; }
+    }
+}
