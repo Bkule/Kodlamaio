612a87913a7e5f73432387fc0a3c9dae58b493ee
Berkin Kule <berkinkule@gmail.com>
2021-04-06 14:15:50 +0300

    OOP Intro

---
+++ Day4/MyDictionary/MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T1,T2>
+    {
+        T1[] keys;
+        T1[] _tempKey;
+        T2[] values;
+        T2[] _tempValue;
+
+        public MyDictionary()
+        {
+            keys = new T1[0];
+            values = new T2[0];
+        }
+
+        public void Add(T1 key, T2 value)
+        {
+            _tempKey = keys;
+            _tempValue = values;
+
+            keys = new T1[keys.Length + 1];
+            values = new T2[values.Length + 1];
+
+            for (int i = 0; i < _tempKey.Length; i++)
+            {
+                keys[i] = _tempKey[i];
+            }
+            for (int i = 0; i < _tempValue.Length; i++)
+            {
+                values[i] = _tempValue[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+    }
+}
---
+++ Day4/MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  
+            (Türkçe)
+            (Aynen derste yaptığımız MyList gibi siz de kendi Dictonary sınıfınızı yazınız. (MyDictionary) Sadece ekleme(Add) metodunu yazınız.)
+                
+                
+            (English)
+            (Write your own Dictonary class, just like the MyList we did in the lesson. (MyDictionary) Just type in the method of add.)
+            */
+
+
+            MyDictionary<string, string> dictionary = new MyDictionary<string, string>();
+            dictionary.Add("qwe", "123");
+        }
+    }
+}
